6ad88d9d03c2ceccb0d99891e8f3ba4ea12ef94f
Piyush Kuhad <piyushkuhad@gmail.com>
2019-06-26 18:55:57 +0530

    Updates

---
+++ precache-manifest.002614a2c341ab2453112740e7cb3c86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3054566c63be86225f6fb0636c573124",
+    "url": "/face-detection-app/index.html"
+  },
+  {
+    "revision": "830b30c7ae7b8fc6cbd6",
+    "url": "/face-detection-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "309036620b2aca50b59a",
+    "url": "/face-detection-app/static/css/main.ea4d859e.chunk.css"
+  },
+  {
+    "revision": "830b30c7ae7b8fc6cbd6",
+    "url": "/face-detection-app/static/js/2.a196f64e.chunk.js"
+  },
+  {
+    "revision": "309036620b2aca50b59a",
+    "url": "/face-detection-app/static/js/main.07cad259.chunk.js"
+  },
+  {
+    "revision": "2cc03c80c8f5fe75b687",
+    "url": "/face-detection-app/static/js/runtime~main.a1605b1a.js"
+  },
+  {
+    "revision": "c114cde2d99e18cd281b24f913ca0a82",
+    "url": "/face-detection-app/static/media/Logo.c114cde2.png"
+  }
+]);
